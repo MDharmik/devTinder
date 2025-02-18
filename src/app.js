bbb504e10fc817edd3f196745a9640d98f0970d3
madhura01 <madhura.dharmik@wizgle.com>
2025-02-18 22:16:19 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,44 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res)=> {
-    res.send("Hello from the dashboard!");
-})
+// app.use("/hello/2", (req, res)=> {
+//     res.send("Hello again");
+// });
 
-app.use("/hello", (req, res)=> {
-    res.send("Hello! Hello! Hello!");
-})
+// app.use("/hello", (req, res)=> {
+//     res.send("Hello! Hello! Hello!");
+// });
 
+// app.use("/test", (req, res)=> {
+//     res.send("Hello from the Test Server!");
+// });
+
+// app.use("/", (req, res)=> {
+//     res.send("Hello from the dashboard!");
+// });
+
+// ==========================================================================================
+
+// This will only handle GET call to /user 
+app.get("/user", (req, res) => {
+    res.send({firstName:'Madhura', lastName:'Dharmik'});
+});
+
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the DB");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully");
+});
+
+
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res)=> {
     res.send("Hello from the Test Server!");
 });
+
 
 app.listen(7777, ()=> {
     console.log("Server is successfully listening on port 7777...");
--- src/homework.md
+++ src/homework.md
@@ -6,3 +6,13 @@ listen to port 7777
 write request handlers for /test, /hello
 install nodemon and update inside package.json
 differerence between ~ and ^
+==================================
+
+Initialize git
+add .gitignore
+create remote repo on github
+push all code to remote origine
+play with routes and route extension ex.- /hello, /hello/2, /test
+order of the routes matter a lot
+install postman and make a workspace/collection > test API call
+Write logic to handle GET, POST, DELETE, PATCH api calla and test them on Postman
