fb1f3daaf2cf7436dcd465e6f6ad6abb34c39746
madhura01 <madhura.dharmik@wizgle.com>
2025-03-10 12:44:10 +0530

    Revert "cookies, jwt-token"
    This reverts commit 9db307980bf36d936102ca79b9ef16fb97c16c5c.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,32 +1,26 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-
-const userAuth = async (req, res, next) => {
-  try {
-    // Read the token
-    const { token } = req.cookies;
-
-    if (!token) {
-      throw new Error("Token is not valid");
+const adminAuth =  (re, res, next)=> {
+    console.log("Admin auth is getting checked!");
+    const token = 'xyz';
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized){
+        res.status(401).send('unauthorized reuest');
+    }else {
+        next();
     }
+}
 
-    // Verify the token
-    const decodedObj = await jwt.verify(token, "DEV@Tinder$2025");
-    const { _id } = decodedObj;
-
-    // Find the user
-    const user = await User.findById(_id);
-    if (!user) {
-      throw new Error("User not found");
+const userAuth =  (req, res, next)=> {
+    console.log("User auth is getting checked!");
+    const token = 'abc';
+    const isUserAuthorized = token === "abc";
+    if(!isUserAuthorized){
+        res.status(401).send('unauthorized reuest');
+    }else {
+        next();
     }
-
-    req.user = user;
-    next();
-  } catch(err) {
-    res.status(400).send("ERROR: " + err.message);
-  }
-};
+}
 
 module.exports = {
-  userAuth,
-};
+    adminAuth,
+    userAuth
+}
